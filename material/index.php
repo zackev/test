84a7c355c545648bba642d754cd7e98af5c87225
MUHAMMAD ZACKY MAULANA RAHMAN <83176413+zackev@users.noreply.github.com>
2022-03-21 02:54:27 +0700

    Add files via upload

---
+++ material/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+
+    <!-- FONT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="backgr">
+        <div class="left">
+            <h1>LATEST<br>DESIGN BLOGS</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button>LEARN MORE</button>
+        </div>
+    </div>
+    <img class="img" src="../img/Screenshot_1.png" alt="">
+</body>
+</html>
